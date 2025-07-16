f1c2f48c40762cac2a5d5654f2d77b27b087e90e
martin_lednar_ledo <martin.lednar@riesenia.com>
2025-07-16 13:50:31 +0200

    RSHOP-882: Add index.js with exports of the configs (#3)

---
+++ index.js
@@ -0,0 +1,5 @@
+import baseEslintConfig from "./config/eslintrc.js";
+import basePrettierConfig from "./config/prettierrc.js";
+import baseStylelintConfig from "./config/stylelintrc.js";
+
+export {baseEslintConfig, basePrettierConfig, baseStylelintConfig};
